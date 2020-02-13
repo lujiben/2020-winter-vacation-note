3a88705a4b68f14b3846347e994f89391313e18d
lujiben <937271985@qq.com>
2020-02-13 11:18:19 +0800

    完善导航栏功能；更新笔记

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
 $(function () {
-  $("a[href='#']").click(function (e) {
-    // 禁止默认事件
-    e.preventDefault();
-    return false;
-  })
 
-  $('.about').click(function() {
+  // $("a[href*='#']").click(function (e) {
+  //   // 禁止默认事件
+  //   e.preventDefault();
+  //   return false;
+  // })
+
+  $('.nav a').click(function(e) {
     // 获取当前窗口的高度
     var winH = $(window).height();
-    $('body,html').animate({
-      scrollTop: winH
-    },600,function(){
+    // 获取自定义属性
+    var target = $(this).attr('data-index');
+    var finalH = 0;
+    finalH = winH * target;
 
+    $('body,html').animate({
+      scrollTop: finalH
+      },600,function(){
     })
+    e.preventDefault();
+    return false;
   })
 
 })
