779eedd534faf7e49baa46d82e6b9e741cb25003
lujiben <937271985@qq.com>
2020-02-14 15:17:36 +0800

    增加点击按钮后的处理逻辑

--- js/index.js
+++ js/index.js
@@ -22,8 +22,28 @@ $(function () {
     return false;
   })
 
+  // 点击切换按钮
   $('.scroll-tip').click(function () {
-    $('.skill-section .fade').addClass('fade-in');
+    var index = $(this).attr('data-index');
+    var winH = $(window).height();
+    var name = $(this).attr('next-name');
+    if(index < 5) {
+      // 点击的是前四个按钮
+      // 通过按钮的next-name属性，获取下一部分的class值，并给他加上fade-in这个class，触发过渡效果
+      $('body,html').animate({
+        scrollTop: winH * index
+      },600, function() {})
+      if(name) {
+        $(name).find('.fade').addClass('fade-in');
+      }
+    }else {
+      // 点击的是最后一个按钮
+      // 返回顶部，并且移除各个部分的的fade-in这个class，方便再次展示效果
+      $('body,html').animate({
+        scrollTop: 0
+      },600, function() {})
+      $('.fade').removeClass('fade-in')
+    }
   })
 
 })
