c6b0a91b246b85f0abfe7420c700d78e2e85316b
lujiben <937271985@qq.com>
2020-02-15 11:16:37 +0800

    优化页面滚动的方式；实现导航栏固定

--- js/index.js
+++ js/index.js
@@ -11,13 +11,26 @@ $(function () {
     var winH = $(window).height();
     // 获取自定义属性
     var target = $(this).attr('data-index');
+    // 获取该模块的名字，增加过渡效果
+    var name = $(this).attr('section-name');
     var finalH = 0;
     finalH = winH * target;
-
-    $('body,html').animate({
-      scrollTop: finalH
-      },600,function(){
-    })
+    // target ? viewScroll('body,html', finalH, true) : viewScroll('body,html', finalH, false);
+    // =================
+    // if(target) {
+    //   // target为true时，说明点击了1234，此时需要固定
+    //   viewScroll('body,html', finalH, true)
+    // }else {
+    //   // 点击了home
+    //   viewScroll('body,html', finalH, false)
+    // }
+    // 通过target * 1把str类型的数据转为num，方便后续判断
+    // target为0，表示回到首页，导航栏不固定，对应false
+    // target为1234时，表示向下滑动，导航栏固定，对应true
+    viewScroll('body,html', finalH, target * 1);
+    if(name) {
+      $(name).find('.fade').addClass('fade-in');
+    }
     e.preventDefault();
     return false;
   })
@@ -30,20 +43,35 @@ $(function () {
     if(index < 5) {
       // 点击的是前四个按钮
       // 通过按钮的next-name属性，获取下一部分的class值，并给他加上fade-in这个class，触发过渡效果
-      $('body,html').animate({
-        scrollTop: winH * index
-      },600, function() {})
+      viewScroll('body,html', winH * index, true);
       if(name) {
         $(name).find('.fade').addClass('fade-in');
       }
     }else {
       // 点击的是最后一个按钮
       // 返回顶部，并且移除各个部分的的fade-in这个class，方便再次展示效果
-      $('body,html').animate({
-        scrollTop: 0
-      },600, function() {})
-      $('.fade').removeClass('fade-in')
+      viewScroll('body,html', 0, false);
+      $('.fade').removeClass('fade-in');
     }
   })
+
+  // 统一同理滚动,el:要滚动的元素，height：滚动的值,fixed:是否需要固定导航栏
+  function viewScroll(el, height, fixed) {
+    $(el).animate({
+      scrollTop: height
+    },600, function() {
+      // 判断是否需要固定导航栏
+      fixedNav(fixed)
+    })
+  }
+  
+  // 固定导航栏,flag表示是否需要固定
+  function fixedNav(flag) {
+    if(flag) {
+      $('.section-header .nav').addClass('fixed');
+    }else {
+      $('.section-header .nav').removeClass('fixed');
+    }
+  }
 
 })
